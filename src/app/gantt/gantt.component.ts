4f2145cbf377690e6ab864f78c31f5a71f469e93
Marek Sławiński <marek.slawinski@live.com>
2018-02-02 12:56:56 +0100

    Basic init

---
+++ src/app/gantt/gantt.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'gantt',
+  templateUrl: './gantt.component.html',
+  styleUrls: ['./gantt.component.scss']
+})
+export class GanttComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
